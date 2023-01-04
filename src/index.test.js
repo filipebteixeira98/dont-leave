1fdcffcd15e30c5e588fc403feace7ef539f484f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-01-04 17:51:18 -0300

    test: should not run the callback function before the delay (used jest timer mocks)

--- src/index.test.js
+++ src/index.test.js
@@ -2,24 +2,37 @@ import DontLeave from './index'
 
 describe('DontLeave', () => {
   let callback, onLeaveIntent
+  const delay = 1000
+
+  jest.useFakeTimers()
 
   beforeEach(() => {
     callback = jest.fn()
-    onLeaveIntent = new DontLeave(callback)
+    onLeaveIntent = new DontLeave(callback, delay)
   })
 
   it('should run the callback function if the user goes out of the screen', () => {
-    // simulate the user leaving the page
+    jest.advanceTimersByTime(delay)
+
     document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
 
     expect(callback).toHaveBeenCalled()
   })
 
   it('should not run the callback function if the user stills on the screen', () => {
-    // simulate the user standing on the screen
+    jest.advanceTimersByTime(delay)
+
     document.dispatchEvent(
       new MouseEvent('mouseout', { relatedTarget: new EventTarget() })
     )
+
+    expect(callback).not.toHaveBeenCalled()
+  })
+
+  it('should not run the callback function before the delay', () => {
+    jest.advanceTimersByTime(delay / 2)
+
+    document.dispatchEvent(new MouseEvent('mouseout', { relatedTarget: null }))
 
     expect(callback).not.toHaveBeenCalled()
   })
